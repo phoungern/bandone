8e065abd36c59c0631a94b063374efa4b2a617da
phou <phonesavanhphoungern1995@gmail.com>
2018-08-06 23:00:07 +0700

    Initial commit

---
+++ app/src/main/java/com/weebly/laosbandone/www/bandone/phoungern.java
@@ -0,0 +1,13 @@
+package com.weebly.laosbandone.www.bandone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class phoungern extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phoungern);
+    }
+}
